288c4fd1482408bbae7db25f3f2d7dc42b9bcdde
Nin2ty <50512444+KooEric@users.noreply.github.com>
2022-05-11 13:24:56 +0900

    Delete mrc_test.py

--- BERT/mrc_test.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-question_answering = pipeline("question-answering", model="ainize/klue-bert-base-mrc", tokenizer="ainize/klue-bert-base-mrc")
-content = open('content_utf8.txt', 'rt', encoding='utf-8').read()
-print("Content\n" + content)
-question = '자택과 집무실 간 이동 거리는 얼마인가?'
-print("Question:\n" + question)
-result = question_answering(question=question, context=content)
-print("Answer:{}, Score: {}".format(result['answer'], round(result['score'],3)))
